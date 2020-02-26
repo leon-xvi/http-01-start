c614d86453d4c9e465102007027d18e063e5920a
Justin Walker <walkerjt93@gmail.com>
2020-02-26 16:49:15 -0500

    modifying request objects

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -7,6 +7,9 @@ import {
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     console.log(`Request type is ${req.method}`);
-    return next.handle(req);
+    const modifiedRequest = req.clone({
+      headers: req.headers.append('Auth', 'xyz')
+    });
+    return next.handle(modifiedRequest);
   }
 }
