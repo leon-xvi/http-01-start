a818bbe8113220703ccef26038002559895c3086
Justin Walker <walkerjt93@gmail.com>
2020-02-26 12:55:12 -0500

    displaying a loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Post } from './post.model';
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   firebaseUrl = 'https://ng-complete-guide-f08c4.firebaseio.com/posts.json';
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -37,6 +38,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>(this.firebaseUrl)
       .pipe(
@@ -51,6 +53,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe(posts => {
+        this.isFetching = false;
         this.loadedPosts = posts;
       });
   }
