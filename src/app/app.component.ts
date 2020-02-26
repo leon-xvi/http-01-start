b16940f41e55441ca6a049aa50e4ad5ad2201645
Justin Walker <walkerjt93@gmail.com>
2020-02-26 11:33:06 -0500

    outputting posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Post } from './post.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
   firebaseUrl = 'https://ng-complete-guide-f08c4.firebaseio.com/posts.json';
 
   constructor(private http: HttpClient) {}
@@ -51,7 +51,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe(posts => {
-        console.log(posts);
+        this.loadedPosts = posts;
       });
   }
 }
